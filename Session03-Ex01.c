aec06596960907f48e7adaa0d3bbb2c83366ea25
Nguyễn Văn Tuấn Nam <nn1307634@gmail.com>
2024-11-21 19:36:51 +0700

    Upload bài tập session03-Ex01

---
+++ Session03-Ex01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+	int main(void){
+		char ten[50];
+	    printf("Hay nhap ten: ");
+        scanf("%s",&ten);
+        printf("Xin chao %s\n",ten);
+         	
+	    return 0;
+	}
+
